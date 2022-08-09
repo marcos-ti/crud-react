8e252c42f432db11d66caf0eea8996d8d4f3acc9
Marcos Antônio Vieira Faria <marcosfaria@IN.IBICT.BR>
2022-08-09 14:35:40 -0300

    Backend / Frontend

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,7 @@
+import "./Footer.css";
+import React from "react";
+
+const footer = () => {
+  return <footer className="footer">Rodapé</footer>;
+};
+export default footer;
---
+++ frontend/src/components/template/Logo.jsx
@@ -0,0 +1,6 @@
+import "./Logo.css";
+import React from "react";
+
+export default function logo() {
+  return <aside className="logo">Logo</aside>;
+}
