d3a339f2ae792f8303e38b3bc7457bc402491cdb
Marcos Antônio Vieira Faria <marcosfaria@IN.IBICT.BR>
2022-08-10 10:21:33 -0300

    Estilizando o Conteúdo

---
+++ frontend/src/components/template/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../Main";
+
+export default (props) => (
+  <Main icon="home" title="Início" subtitle="Projeto React.">
+    <div className="display-4">Bem vindo!</div>
+    <hr />
+    <p className="mb-0">Sistema de cadastro desenvolvido em React</p>
+  </Main>
+);
