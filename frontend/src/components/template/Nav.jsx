267569114180642a45b499273d74819ae9d5afe1
Marcos Antônio Vieira Faria <marcosfaria@IN.IBICT.BR>
2022-08-10 10:04:24 -0300

    Estilizando o Menu

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,7 +1,17 @@
-import './Nav.css'
-import React from 'react'
+import "./Nav.css";
+import React from "react";
 
-export default props =>
-    <aside className="menu-area">
-        Navegação
-    </aside>
+export default (props) => (
+  <aside className="menu-area">
+    <nav className="menu">
+      {/* Refatorar : cria componente: nav-item.jsx, depois importa pra nav,jsx e depois faz tipo assim: <i className={`fa fa-${props.icon}`}></i> {props.title}
+       */}
+      <a href="#/">
+        <i className="fa fa-home"></i> Início
+      </a>
+      <a href="#/users">
+        <i className="fa fa-users"></i> Usuário
+      </a>
+    </nav>
+  </aside>
+);
