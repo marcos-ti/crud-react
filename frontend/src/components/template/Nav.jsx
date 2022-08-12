d3cbf3f4e906d6f125822ae5cd891fafd52a58c4
Marcos <saoroque@hotmail.com>
2022-08-11 21:20:20 -0300

    Rotas

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,17 +1,18 @@
 import "./Nav.css";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default (props) => (
   <aside className="menu-area">
     <nav className="menu">
       {/* Refatorar : cria componente: nav-item.jsx, depois importa pra nav,jsx e depois faz tipo assim: <i className={`fa fa-${props.icon}`}></i> {props.title}
        */}
-      <a href="#/">
+      <Link to="/">
         <i className="fa fa-home"></i> Início
-      </a>
-      <a href="#/users">
+      </Link>
+      <Link to="/users">
         <i className="fa fa-users"></i> Usuário
-      </a>
+      </Link>
     </nav>
   </aside>
 );
